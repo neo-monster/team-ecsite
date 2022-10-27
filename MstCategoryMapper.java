818e701ed3b05257b02d090f89465959270b1d42
neo-monster <p.monster_neo@icloud.com>
2022-10-27 15:17:55 +0900

    team top-page

---
+++ MstCategoryMapper.java
@@ -0,0 +1,26 @@
+package jp.co.internous.duo.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import jp.co.internous.duo.model.domain.MstCategory;
+
+
+/**
+ * mst_categoryにアクセスするDAO
+ * @author インターノウス
+ *
+ */
+@Mapper
+public interface MstCategoryMapper {
+	
+	/**
+	 * カテゴリー情報を取得する
+	 * @return カテゴリー情報リスト
+	 */
+	@Select("SELECT * FROM mst_category")
+	List<MstCategory> find();
+
+}
---
+++ insert.sql
@@ -0,0 +1,37 @@
+insert into mst_category
+(category_name)
+values ('ボリュームアップ');
+insert into mst_category
+(category_name)
+values ('カラーケア');
+insert into mst_category
+(category_name)
+values ('スカルプケア');
+
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('コアミー シャンプー 250ml','こあみー しゃんぷー 250ml','1','3520','/img/coreme_shampoo.JPG');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('コアミー マスク 235g','こあみー ますく 235g','1','3740','/img/coreme_mask.JPG');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('エミーム ボリュームアップミスト','えみーむ ぼりゅーむあっぷみすと','1','2640','/img/etmeme.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('ケラスターゼ ブロンドアブソリュ シャンプー','けらすたーぜ ぶろんどあぶそりゅ しゃんぷー','2','3740','/img/kera_shampoo.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('ケラスターゼ ブロンドアブソリュ マスク','けらすたーぜ ぶろんどあぶそりゅ ますく','2','5720','/img/kera_mask.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('ケラスターゼ ブロンドアブソリュ オイル','けらすたーぜ ぶろんどあぶそりゅ おいる','2','4400','/img/kera_oil.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('ジオ スキャルプシャンプー','じお すきゃるぷしゃんぷー','3','1980','/img/theo.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('ジオ スキャルプシャンプー アイスミント','じお すきゃるぷしゃんぷー あいすみんと','3','1980','/img/theo_ice.jpg');
+insert into mst_product
+(product_name,product_name_kana,category_id,price,image_full_path)
+values ('モナリ ミハツシャンプー','もなり みはつしゃんぷー','3','5500','/img/monnali.jpg');
